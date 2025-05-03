bc56ec3c6ec19be95a697eb1b0353999877c92bd
EFFz0 <arlingrose@gmail.com>
2025-05-02 21:30:44 -0400

    Add files via upload

--- src/js/data/2021-05-09.js
+++ src/js/data/2021-05-09.js
@@ -98,7 +98,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "RB-79 Ball",
     img: "Y5bJH9G.jpg",
-    opts: { series: ["0079", "08MS", "MSGT", "0083"]}
+    opts: { series: ["0079", "08MS", "MSGT", "0083", "ARMOR"]}
   },
   {
     name: "MS-05B Zaku I",
@@ -108,7 +108,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "MS-06F Zaku II",
     img: "rg0RyoP.jpg",
-    opts: { series: ["0079", "08MS", "MSGT", "0083"]}
+    opts: { series: ["0079", "08MS", "MSGT", "0083", "MSGZZ"]}
   },
   {
     name: "Char's MS-06S Zaku II Commander Type",
@@ -123,7 +123,7 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "MS-09B Dom",
     img: "3dBvjgz.jpg",
-    opts: { series: ["0079", "08MS", "MSGT", "0083"]}
+    opts: { series: ["0079", "08MS", "MSGT", "0083", "MSGZZ"]}
   },
   {
     name: "MS-14A Gelgoog",
@@ -173,36 +173,244 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "MA-08 Big Zam",
     img: "lsi8FPz.jpg",
-    opts: { series: ["0079"]}
+    opts: { series: ["0079", "ARMOR"]}
   },
   {
     name: "MA-04X Zakrello",
     img: "siUKpKj.jpg",
-    opts: { series: ["0079"]}
+    opts: { series: ["0079", "ARMOR"]}
   },
   {
     name: "MA-05 Bigro",
     img: "Q69B8WR.jpg",
-    opts: { series: ["0079"]}
+    opts: { series: ["0079", "ARMOR"]}
   },
   {
     name: "MAM-07 Grublo",
     img: "rqo7R5m.jpg",
-    opts: { series: ["0079"]}
+    opts: { series: ["0079", "ARMOR"]}
   },
   {
     name: "MAN-03 Braw Bro",
     img: "5yaT3FE.jpg",
-    opts: { series: ["0079"]}
+    opts: { series: ["0079", "ARMOR"]}
   },
   {
     name: "MAN-08 Elmeth",
     img: "VYCun8X.jpg",
-    opts: { series: ["0079"]}
+    opts: { series: ["0079", "ARMOR"]}
   },
   {
     name: "MAX-03 Adzam",
     img: "EUsDrTQ.jpg",
-    opts: { series: ["0079"]}
+    opts: { series: ["0079", "ARMOR"]}
+  },
+  
+  
+  
+  {
+    name: "MSZ-006 Zeta Gundam",
+    img: "VqQA5dy.jpg",
+    opts: { series: ["MSZG", "MSGZZ"]}
+  },
+  {
+    name: "RX-178 Gundam Mk-II (AEUG)",
+    img: "L5drnlU.jpg",
+    opts: { series: ["MSZG", "MSGZZ"]}
+  },
+  {
+    name: "RX-178 Gundam Mk-II (Titans)",
+    img: "jjVJY2o.jpg",
+    opts: { series: ["MSZG", "PAINT"]}
+  },
+  {
+    name: "MSN-00100 Hyaku Shiki",
+    img: "wfgYZJQ.jpg",
+    opts: { series: ["MSZG", "MSGZZ"]}
+  },
+  {
+    name: "Petite-Mobile",
+    img: "NX33g1t.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "RMS-099 Rick Dias",
+    img: "IgT6zeR.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "MSA-005 Methuss",
+    img: "fks1XXP.jpg",
+    opts: { series: ["MSZG", "MSGZZ"]}
+  },
+  {
+    name: "MSA-003 Nemo",
+    img: "VxB4htD.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "MSK-008 Dijeh",
+    img: "1M5wWpS.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "RMS-179 GM II (EFF)",
+    img: "w4UTXrk.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "RMS-179 GM II (AEUG)",
+    img: "JZsOE85.jpg",
+    opts: { series: ["MSZG", "PAINT"]}
+  },
+  {
+    name: "MRX-009 Psycho Gundam",
+    img: "Q1uwnj1.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "MRX-010 Psycho Gundam Mk-II",
+    img: "HZrn2hA.jpg",
+    opts: { series: ["MSZG", "MSGZZ"]}
+  },
+  {
+    name: "PMX-003 The O",
+    img: "vDW4EEj.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "PMX-001 Palace Athene",
+    img: "FUGGIQ0.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "PMX-002 Bolinoak Sammahn",
+    img: "Kop8zyF.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "RGC-80 GM Cannon (Jaburo)",
+    img: "7PUinAf.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "MS-06V Zaku Tank",
+    img: "BTnzsfK.jpg",
+    opts: { series: ["MSZG", "MSGZZ"]}
+  },
+  {
+    name: "MS-06E Zaku Recon Type (EFF)",
+    img: "mzAPpdW.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "MS-06M Marine Hizack (EFF)",
+    img: "6wXF6gN.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "MS-07H Gouf Flight Test Type (EFF)",
+    img: "FP1CbpI.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "MS-11 Act Zaku",
+    img: "lFcqK23.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "NRX-044 Asshimar",
+    img: "S9gZc0o.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "NRX-055 Baund Doc",
+    img: "6MVSCFL.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "NRX-055-1 Baund Doc",
+    img: "fxvHvOl.jpg",
+    opts: { series: ["MSZG", "PAINT"]}
+  },
+  {
+    name: "ORX-005 Gaplant",
+    img: "7pHolzn.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "PMX-000 Messala",
+    img: "HujB9Kq.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "RGM-79SC GM Sniper Custom",
+    img: "ybWeOnw.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "RMS-106 Hizack (Titans)",
+    img: "39Siuus.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "RMS-106 Hizack (EFF)",
+    img: "f0LRKMd.jpg",
+    opts: { series: ["MSZG", "PAINT"]}
+  },
+  {
+    name: "RMS-106CS Hizack Custom",
+    img: "nd4THmT.jpg",
+    opts: { series: ["MSZG", "PAINT"]}
+  },
+  {
+    name: "RMS-108 Marasai",
+    img: "vn6XUA2.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "RMS-117 Galbaldy β",
+    img: "zR6Sdr0.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "RMS-154 Barzam",
+    img: "g6nV6x9.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "RMV-1 Guntank II",
+    img: "HjP7zBx.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "RX-77-3 Guncannon Heavy Custom",
+    img: "w8Kpmrg.jpg",
+    opts: { series: ["MSZG", "PAINT"]}
+  },
+  {
+    name: "RX-110 Gabthley",
+    img: "23ltYDK.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "RX-139 Hambrabi",
+    img: "sYgN9OA.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "RX-160 Byarlant",
+    img: "ZK2O1NS.jpg",
+    opts: { series: ["MSZG"]}
+  },
+  {
+    name: "AMX-003 Gaza-C",
+    img: "WZLsyxc.jpg",
+    opts: { series: ["MSZG", "MSGZZ"]}
+  },
+  {
+    name: "AMX-004 Qubeley",
+    img: "r36t2xr.jpg",
+    opts: { series: ["MSZG", "MSGZZ"]}
   },
 ];
